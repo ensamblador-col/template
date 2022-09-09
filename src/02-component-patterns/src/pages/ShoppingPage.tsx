1e20ff06229ea54762cb2cefec3ddffb65e8f1e0
juanCamilo <juanmu@bextsa.com>
2022-09-09 17:56:15 -0500

    Style: Added css styles to main component

--- src/02-component-patterns/src/pages/ShoppingPage.tsx
+++ src/02-component-patterns/src/pages/ShoppingPage.tsx
@@ -1,5 +1,4 @@
 import {ProductButtons, ProductCard, ProductImage, ProductTitle} from "../components";
-import '../styles/custom-styles.css'
 import {products} from "../data/products";
 
 const product = products[0];
@@ -12,25 +11,18 @@ export const ShoppingPage = () => {
             <br/>
             <ProductCard
                 product={product}
-                className="bg-dark"
                 key={product.id}
                 initialValues={{
                    count: 3,
                    maxCount: 10
-            }}
+                }}
             >
                 {
                     ({reset, count, handleCounter, maxCount, isMaxCountReached}) => (
                         <>
-                            <ProductImage className="custom-image"/>
-                            <ProductTitle className="text-white"/>
-                            <ProductButtons className="btn-dark-mode"/>
-                            <button style={{margin:"3px"}} onClick={() => handleCounter(-2)}> -2 </button>
-                            {
-                                (!isMaxCountReached && <button style={{margin: "3px"}} onClick={() => handleCounter(2)}> +2 </button>)
-                            }
-                            <button style={{margin:"3px"}} onClick={reset}>Reset</button>
-                            <span style={{marginLeft:"20px", fontWeight:"bolder"}}>{count}</span>
+                            <ProductImage />
+                            <ProductTitle />
+                            <ProductButtons />
                         </>
                     )
                 }
